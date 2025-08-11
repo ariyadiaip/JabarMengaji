424b82ba7291c24d1304d9c3bc58ba101fb2f14b
Aip Ariyadi <ariyadiaip@gmail.com>
2025-08-11 15:55:09 +0700

    Update Date

--- app/src/main/java/edu/ariyadi/jabarmengaji/MyApp.kt
+++ app/src/main/java/edu/ariyadi/jabarmengaji/MyApp.kt
@@ -4,8 +4,8 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 /*
-* Created at 04 Juli 2025
-* Updated at 09 Agustus 2025
+* Created at 04 Agustus 2025
+* Updated at 11 Agustus 2025
 * NIM   : 10122901
 * Nama  : Aip Ariyadi
 * Kelas : IF-13
